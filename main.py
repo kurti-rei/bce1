a5ab8f0eaf998a80fa35229406e05c2385ae3498
kurti-rei <kurti.rei@cit.edu.al>
2026-01-06 09:31:30 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+#Taking input from the user
+#Conditional Statements
+#Github
+
+#Comments
+
+#Taking an input from the user
+age = int(input("Please enter your age: "))
+
+#Conditional Statements
+#Ticket Price Checker
+if age > 18:
+    print("You pay 500 ALL")
+elif age > 10:
+    print("You pay 200 ALL")
+else:
+    print("You can enter for free")
+
+
+
+if age > 18:
+    print("You are an adult")
+elif age > 20:
+    print("You are a big adult")
+else:
+    print("You are a child")
